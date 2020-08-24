186e6743deea1de1d9c7d0d48b64bacd729708f1
sectasy0 <sectasy0@gmail.com>
2020-08-24 10:50:15 +0200

    improv: main vm functionality.

--- lc3.c
+++ lc3.c
@@ -48,4 +48,70 @@ enum {
 };
 
 
-int main(int argc, const char* argv[]) {}
+int main(int argc, const char* argv[]) {
+    /* { LOAD ARGUMENTS } */
+    /* { SETUP } */
+
+    /* set the PC to starting pos */
+    /* 0x3000 is the default */
+    enum { PC_START = 0x3000 };
+    reg[R_PC] = PC_START;
+
+    int running = 1;
+    while (running) {
+        /* FETCH */
+        uint16_t instr = mem_read(reg[R_PC]++);
+        uint16_t op = instr >> 12;
+
+        switch (op) {
+            case OP_ADD:
+                /* ADD */
+                break;
+            case OP_AND:
+                /* AND */
+                break;
+            case OP_NOT:
+                /* NOT */
+                break;
+            case OP_BR:
+                /* BR */
+                break;
+            case OP_JMP:
+                /* JMP */
+                break;
+            case OP_JSR:
+                /* JSR */
+                break;
+            case OP_LD:
+                /* LD */
+                break;
+            case OP_LDI:
+                /* LDI */
+                break;
+            case OP_LDR:
+                /* LDR */
+                break;
+            case OP_LEA:
+                /* LEA */
+                break;
+            case OP_ST:
+                /* ST */
+                break;
+            case OP_STI:
+                /* STI */
+                break;
+            case OP_STR:
+                /* STR */
+                break;
+            case OP_TRAP:
+                /* TRAP */
+                break;
+            case OP_RES:
+            case OP_RTI:
+            default:
+                /* BAD OPCODE*/
+                break;
+        }
+    }
+    /* SHUTDOWN */
+}
