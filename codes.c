9dfbc7fa86c74146c7b19952b614db27366c2384
sectasy0 <sectasy0@gmail.com>
2020-08-24 14:57:31 +0200

    refactor: moving codes to another file

---
+++ codes.c
@@ -0,0 +1,50 @@
+/* Registers */
+extern enum {
+    R_R0 = 0,
+    R_R1,
+    R_R2,
+    R_R3,
+    R_R4,
+    R_R5,
+    R_R6,
+    R_R7,
+    R_PC,
+    R_COND,
+    R_COUNT
+};
+
+/* Opcodes */
+extern enum {
+    OP_BR = 0,
+    OP_ADD,
+    OP_LD,
+    OP_ST,
+    OP_JSR,
+    OP_AND,
+    OP_LDR,
+    OP_STR,
+    OP_RTI,
+    OP_NOT,
+    OP_LDI,
+    OP_STI,
+    OP_JMP,
+    OP_RES,
+    OP_LEA,
+    OP_TRAP
+};
+
+/* Condition Flags*/
+extern enum {
+    FL_POS = 1 << 0,
+    FL_ZRO = 1 << 1,
+    FL_NEG = 1 << 2
+};
+// TRAP CODES
+extern enum {
+    TRAP_GETC = 0x20, // get character from keyboard
+    TRAP_OUT = 0x21, // output character
+    TRAP_PUTS = 0x22, // output a word string
+    TRAP_IN = 0x23, // get character from keyboard, echoed onto the terminal
+    TRAP_PUTSP = 0x24, // output a byte
+    TRAP_HALT = 0x25 // halt the program
+};
--- lc3.c
+++ lc3.c
@@ -2,56 +2,7 @@
 #include <stdio.h>
 #include <stdint.h>
 
-/* Registers */
-enum {
-    R_R0 = 0,
-    R_R1,
-    R_R2,
-    R_R3,
-    R_R4,
-    R_R5,
-    R_R6,
-    R_R7,
-    R_PC,
-    R_COND,
-    R_COUNT
-};
-
-/* Opcodes */
-enum {
-    OP_BR = 0,
-    OP_ADD,
-    OP_LD,
-    OP_ST,
-    OP_JSR,
-    OP_AND,
-    OP_LDR,
-    OP_STR,
-    OP_RTI,
-    OP_NOT,
-    OP_LDI,
-    OP_STI,
-    OP_JMP,
-    OP_RES,
-    OP_LEA,
-    OP_TRAP
-};
-
-/* Condition Flags*/
-enum {
-    FL_POS = 1 << 0,
-    FL_ZRO = 1 << 1,
-    FL_NEG = 1 << 2
-};
-// TRAP CODES
-enum {
-    TRAP_GETC = 0x20, // get character from keyboard
-    TRAP_OUT = 0x21, // output character
-    TRAP_PUTS = 0x22, // output a word string
-    TRAP_IN = 0x23, // get character from keyboard, echoed onto the terminal
-    TRAP_PUTSP = 0x24, // output a byte
-    TRAP_HALT = 0x25 // halt the program
-};
+#include "codes.c"
 
 extern uint16_t memory[UINT16_MAX];
 extern uint16_t reg[R_COUNT];
